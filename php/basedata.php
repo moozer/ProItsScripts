57f5cf06161d0589e59b7a3e0a69f4582b38e7fe
moz <moz@local>
2013-11-15 11:28:34 +0100

    added gitignore to exclude backup files updated basedata.php to send conform json data implemented CheckBaseData to fetch data

--- php/basedata.php
+++ php/basedata.php
@@ -1,5 +1,5 @@
 {
-	date: "<?php echo date('Y-m-d H:i:s'); ?>"
-	hostname: "<?php echo gethostname(); ?>"
-	version: "<?php echo phpversion(); ?>"
+	"date": "<?php echo date('Y-m-d H:i:s'); ?>",
+	"hostname": "<?php echo gethostname(); ?>",
+	"version": "<?php echo phpversion(); ?>"
 }
---
+++ scripts/CheckBaseData.py
@@ -0,0 +1,32 @@
+#!/bin/env python
+
+
+import sys
+import urllib2
+import json
+
+
+# command line args.
+if len(sys.argv) < 3:
+    print "usage %s <id> <URL>"%sys.argv[0]
+    exit( 1 )
+
+GroupId = sys.argv[1]
+URL = sys.argv[2]
+
+print '%s: 0: Fetching basedata from %s :'%(GroupId, URL)
+
+# fetch data
+req = urllib2.Request(URL)
+response = urllib2.urlopen(req)
+jsondata = response.read() 
+response.close()
+
+# parsing json
+data = json.loads( jsondata )
+
+print '%s: 10: Data fetched: hostname: %s, php version: %s, timestamp: %s'%(GroupId, data['hostname'], data['version'], data['date'])
+
+
+
+
