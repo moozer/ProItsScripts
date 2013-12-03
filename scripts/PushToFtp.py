7ff589d42e87ffb86affb8c2034cca08a10be627
Sifungurux <Jonas_pedersen@hotmail.com>
2013-12-03 09:40:26 +0100

    Added sftp as a option

--- scripts/PushToFtp.py
+++ scripts/PushToFtp.py
@@ -5,34 +5,47 @@
 from ftplib import FTP, FTP_TLS
 import sys
 import os
+import paramiko
 
 FilesToPut = ['../php/basedata.php']
 
-def Push( FtpServer, Username, Password, uploadlist = FilesToPut, port = 21, passive = False, StartTls = False ):
-	print "Login to %s:%s using %s:%s (%s%s)"%(FtpServer, port, Username, 'xxx', 
+def Push( FtpServer, Username, Password, uploadlist = FilesToPut, port = 21, passive = False, StartTls = False, Sftp = False ):
+        print "Login to %s:%s using %s:%s (%s%s)"%(FtpServer, port, Username, 'xxx', 
 				'passive' if passive else 'active', 
 				'/tls' if StartTls else '')
+                
+        if Sftp:
+            paramiko.util.log_to_file('/tmp/paramiko.log')
+            transport = paramiko.Transport((FtpServer,int(port)))
+            transport.connect(username = Username, password = Password)
+            sftp = paramiko.SFTPClient.from_transport(transport)
+            
+            print "Uploading file"
+  
+            filepath = '../php/basedata.php'
+            localpath = 'basedata.php'
+            sftp.put(filepath, localpath)
+            sftp.close()
+            transport.close()
 
-	if StartTls:
-		ftp = FTP_TLS()
-	else:
-		ftp = FTP()
-	
-	#ftp.set_debuglevel(2)
+        else:
+            if StartTls:
+                ftp = FTP_TLS()
+	        else:
+                ftp = FTP()
 
-	ftp.connect( FtpServer, port )
-	ftp.login( Username, Password )                     # user anonymous, passwd anonymous@
-	ftp.set_pasv( passive )
+            ftp.connect( FtpServer, port )
+	        ftp.set_pasv( passive )
 
-	if StartTls:
-		ftp.prot_p()	    
+	        if StartTls:
+                ftp.prot_p()
 
-	for f in uploadlist:
-		print "uploading %s"%f		
-		fp = open( f, 'rb')
-		ftp.storbinary('STOR %s'%os.path.basename(f), fp)     # send the file
-
-	ftp.quit()
+            for f in uploadlist:
+            print "uploading %s"%f
+            fp = open( f, 'rb')
+            ftp.storbinary('STOR %s'%os.path.basename(f), fp)     # send the file
+            
+            ftp.quit()
 
 if __name__ == "__main__":
 	if len(sys.argv) < 5:
